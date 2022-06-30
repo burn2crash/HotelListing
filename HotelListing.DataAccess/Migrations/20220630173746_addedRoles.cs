421b546adde81dd0a27fe49980d7a7eb14244d72
Sergio Osuna <burn2crash@hotmail.com>
2022-06-30 10:38:19 -0700

    Added roles to db

---
+++ HotelListing.DataAccess/Migrations/20220630173746_addedRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.DataAccess.Migrations
+{
+    public partial class addedRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "0b43387b-9b1f-451d-941a-19c4b15c81c2", "49ef727e-d4e0-41c1-834d-05085322d11a", "User", "USER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "73b10174-fb83-467e-928f-5876603fc99b", "f818889c-7bcc-462f-a342-a90adff39fb6", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0b43387b-9b1f-451d-941a-19c4b15c81c2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "73b10174-fb83-467e-928f-5876603fc99b");
+        }
+    }
+}
